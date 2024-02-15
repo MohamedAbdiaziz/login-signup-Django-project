6569a5fa5f641c0fedbe78c9066460de2c3f99cb
TestingUser <maxamedcabdicasiis910@gmail.com>
2024-02-15 20:22:31 +0300

    xalinta qeybta logoutka error kii ka jiray

--- website/base/urls.py
+++ website/base/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path,include
-from .views import home,signup
+from .views import home,signup,logout_view
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", home, name="home"),
-    path('logout/', auth_views.LogoutView.as_view(), name='Logout'),
+    path('logout/', logout_view, name='Logout'),
     path("accounts/signup/", signup,name="signup"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
--- website/base/views.py
+++ website/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 
 
 
@@ -13,3 +16,9 @@ def home(request):
 def signup(request):
     form = UserCreationForm()
     return render(request, "registration/signup.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    # Redirect to a specific URL after logout
+    return redirect('/') 
